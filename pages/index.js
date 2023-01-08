fafcf6429d0d139a3b80fb73611c40cf5d522340
Matteo De Pauw <matteodepauw@yahoo.com>
2023-01-08 23:46:19 +0100

    feat(settings): Added search settings UI

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function index() {
   const [recipes, setRecipes] = useState(undefined)
   const [error, setError] = useState("")
   const [info, setInfo] = useState(undefined)
+  const [isOpen, setIsOpen] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -44,6 +45,7 @@ export default function index() {
             ids: ids,
             apiKey: process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY
           })
+          // https://spoonacular.com/food-api/docs
           fetch(getInfoUrl) // Get info for recipes
             .then(res => res.json())
             .then(data => {
@@ -75,6 +77,38 @@ export default function index() {
 
   return (
     <main className='pb-8 min-h-screen'>
+
+      {isOpen &&
+        <>
+          <div className='fixed w-screen h-screen bg-black opacity-75 z-[9998]'
+            onClick={() => setIsOpen(false)}
+          ></div>
+          <div className='fixed w-[60vw] h-[40vw] ml-auto mr-auto left-0 right-0 top-[50%] translate-y-[-50%] bg-white z-[9999] border border-black shadow-inner rounded-xl p-6'>
+            <button onClick={() => setIsOpen(false)} className="fixed top-0 right-0 p-8">
+              <i className="fa-solid fa-xmark fa-2xl"></i>
+            </button>
+            <h1 className='text-2xl mb-6 text-accent'>Advanced search options</h1>
+            <form>
+              <p className='text-xl mb-1 text-textlighter'>Search engine</p>
+              <input defaultChecked type="radio" id="spoonacularApi" value="spoonacularApi" />
+              <label htmlFor="spoonacularApi">SpoonacularApi</label> <br />
+
+              <p className='text-xl mt-6 mb-4 text-accent'>Preferences</p>
+
+              <p className='text-lg mb-1 text-textlighter'>Ingredient prioritization</p>
+              <input defaultChecked type="radio" id="ranking1" value="ranking1" />
+              <label htmlFor="ranking1">Maximize used ingredients</label> <br />
+              <input type="radio" id="ranking2" value="ranking2" />
+              <label htmlFor="ranking2">Minimize missing ingredients</label> <br />
+
+              <p className='text-lg mt-4 mb-1 text-textlighter'>Pantry items</p>
+              <input defaultChecked type="checkbox" id="ignorePantry" value="ignorePantry" />
+              <label htmlFor="ignorePantry">Ignore typical pantry items, such as water, salt, flour, etc.</label>
+            </form>
+          </div>
+        </>
+      }
+
       <div className={`py-16 ${styles.parallax}`}>
         <div className='bg-white rounded-xl flex flex-row justify-between items-center h-[400px] max-w-6xl mx-auto drop-shadow-md'>
           <div className='w-max mx-auto'>
@@ -89,7 +123,7 @@ export default function index() {
               <button type="submit" disabled={loading} className={`bg-accent px-6 py-3 rounded-r-xl text-white hover:text-accent  hover:bg-white duration-75 ease ${styles.innerShadow}`}>Search recipes</button>
             </form>
 
-            <button className='underline text-accent mb-4 mt-1'>Advanced search</button>
+            <button className='underline text-accent mb-4 mt-1' onClick={() => setIsOpen(true)}>Advanced search</button>
 
             <p className='text-textlighter text-sm'>Please separate items with a space.</p>
             <p className='text-textlighter text-sm'>eg. I have eggs and milk. Appropriate search input: Eggs milk</p>
